4ef6374a688f62e677edf82841204e8bc35624bf
rishabhranawat <rishabhranawat12345@gmail.com>
2017-08-27 17:37:43 -0400

    email test

--- test.py
+++ test.py
@@ -1,13 +1,46 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
+
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
+	send_email('rishabhranawat12345', 'zinnialondon!!', 
+		'rishabhranawat12345@gmail.com', 'testing cron job', 'hey rish')
     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
+
+@sched.scheduled_job('email', day_of_week='sun', hour=17, minutes=45)
+def scheduled_email():
+	send_email('rishabhranawat12345', 'zinnialondon!!', 
+		'rishabhranawat12345@gmail.com', 'testing cron job', 'hey rish')
+
 
 sched.start()
